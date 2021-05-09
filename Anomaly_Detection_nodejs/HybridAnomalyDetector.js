291642d6941711eede82baa74ec1a4327ac24141
shmuelyaish <73137368+shmuelyaish@users.noreply.github.com>
2021-05-09 17:42:34 +0300

    Add files via upload

---
+++ Anomaly_Detection_nodejs/HybridAnomalyDetector.js
@@ -0,0 +1,29 @@
+const basic = require("./basic_funcs.js");
+const tseries = require("./timeseries.js");
+const simple = require("./SimpleAnomalyDetector.js");
+const ec = require('smallest-enclosing-circle');
+class HybridAnomalyDetector extends simple.SimpleAnomalyDetector {
+	//learnHelper(const TimeSeries& ts, float p/*pearson*/, string f1, string f2, Point ** ps)
+	learnHelper(ts, p, f1, f2, x, y) {
+		if (p > 0.5 && p < threshold) {
+			var circle = ec(x, y);
+			var c = new simple.correlatedFeatures();
+			c.feature1 = f1;
+			c.feature2 = f2;
+			c.corrlation = p;
+			c.threshold = circle.r * 1.1;
+			c.cx = circle.x;
+			c.cy = circle.y;
+			this.cf.push(c);
+		}
+	}
+	//isAnomalous(float x, float y, correlatedFeatures c)
+	isAnomalous(x, y, c) {
+		var tempX = (c.cx - x) * (c.cx - x);
+		var tempY = (c.cy - y) * (c.cy - y);
+		var dist = Math.sqrt(tempX + tempY);
+		return (c.corrlation >= this.threshold && super.isAnomalous(x, y, c)) ||
+			(c.corrlation > 0.5 && c.corrlation < this.threshold && dist > c.threshold);
+	}
+}
+module.exports = { HybridAnomalyDetector};
