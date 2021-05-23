77752a69f61b4b7de38e4fed3b46e40d42a6ad81
EladBil <73217864+EladBil@users.noreply.github.com>
2021-05-23 14:26:08 +0300

    Add files via upload

--- controller/expServer.js
+++ controller/expServer.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fileUpload = require('express-fileupload');
-const model = require("../model/SearchInFile")
+const model = require("../model/ModelRequests")
+
 
 const app = express()
 app.use(express.urlencoded({
@@ -19,61 +20,119 @@ app.get("/" , (req,res)=>{
     res.sendFile("./index.html")
 })
 
-/*
-app.post("/detect",(req,res)=>{
-    res.write("searching for" + req.body.key +":\n")
-    var key = req.body.key
-    if(req.files){
-        var file = req.files.text_file //name in html of file
-        console.log(file)
-        //file.data
-        var result = model.searchText(key,file.data.toString())
-        res.write(result)
+const LINEAR_ALGOIRTHM_ID = 1;
+const HYBRID_ALGOIRTHM_ID = 2;
+
+app.post('/detect', (req, res) => {
+
+    // Checking if there any files sent
+    if (!req.files) {
+        req.write('You need to upload the files BEFORE submition.\n');
+        res.end();
+        return;
     }
-    res.end()
-} )*/
+
+    const trainigFile = req.files.trainFile;
+    const testingFile = req.files.testFile;
+
+    let messege;
+    
+    if (req.body.AlgorithmType == LINEAR_ALGOIRTHM_ID) {
+        messege = model.simpleAnomaly(trainigFile.data.toString(), testingFile.data.toString());
+    } else {
+        messege = model.hybridAnomaly(trainigFile.data.toString(), testingFile.data.toString());
+    }
+
+    console.log(messege);
+    // console.log(trainigFile);
+    let JSONmessege = {};
+   
+    function mapToObj(map){
+        const obj = {}
+        for (let [k,v] of map)
+          obj[k] = v
+        return obj
+    }
+
+    JSONmessege = mapToObj(messege);
+
+    res.send(JSON.stringify(JSONmessege));    
+
+    res.end();
+});
 
 
-app.post("/detect",(req,res)=>{
+app.post("/detectTrusted",(req,res)=>{
     res.write("searching for" + req.body.key +":\n")
     var key = req.body.key
-    if(req.files){
+    
+    // Checking if there any files sent
+    if (!req.files) {
+        req.write('You need to upload the files BEFORE submition.\n');
+        res.end();
+        return;
+    }
+
+    // console.log(req.baseUrl);
+    console.log(req);
+
+
+    const trainigFile = req.files.trainFile;
+    const testingFile = req.files.testFile;
+
+    let messege;
+    
+    if (req.body.AlgorithmType == LINEAR_ALGOIRTHM_ID) {
+        messege = model.simpleAnomaly(trainigFile.data.toString(), testingFile.data.toString());
+    } else {
+        messege = model.hybridAnomaly(trainigFile.data.toString(), testingFile.data.toString());
+    }
+
+    console.log(messege);
+    // console.log(trainigFile);
+    let JSONmessege = {};
+   
+    function mapToObj(map){
+        const obj = {}
+        for (let [k,v] of map)
+          obj[k] = v
+        return obj
+    }
+
+    JSONmessege = mapToObj(messege);
+
+   
+
         var file = req.files.trainFile //name in html of file
         let jsonString = file.data.toString()
-        let jsonString2 = JSON.parse(jsonString)
-        //file.data
-        //var personJSONString=JSON.stringify(file.data.toString())
-        //var jsonString = '{"some":"json"}';
-        //var temp = '{"0":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"1":[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,00,0,0,0,0,0,0,0]}'
-        //var jsonString2=JSON.parse(file.data.JSON)
-
+        let jsonString2 = JSONmessege;
+        
+        
+        
         let send = ""
         for (var key in jsonString2) {
+            let counter = 1;
             if (jsonString2.hasOwnProperty(key)) {
-                send += 'List of anomalies in feature ' +key + "\n" + jsonString2[key]+"\n";
+                 
+                let arr = jsonString2[key].toString().split(",");
+                send += "Attribute " +key+": \n";
+                let check = 0;
+                for(var i in arr){
+                    if(check === 0){
+                        send +=`anomaly number: ${counter} -> from: ${arr[i]} `
+                        counter++;
+                    }
+                    else{
+                        send +="to: " + arr[i] +"\n";
+                    }
+                   check = (check+1)%2            
+                }
+                send +="\n"
+
             }
         }
         
-
-       /* var jsonreal = JSON.stringify(jsonString.data.toString())
-        var jsonPretty = JSON.stringify(JSON.parse(jsonString),null,2);  
-        
-        var json = jsonreal.items
-        var fields = Object.keys(json[0])
-        var replacer = function(key, value) { return value === null ? '' : value } 
-        var csv = json.map(function(row){
-        return fields.map(function(fieldName){
-            return JSON.stringify(row[fieldName], replacer)
-        }).join(',')
-        })
-        csv.unshift(fields.join(',')) // add header column
-        csv = csv.join('\r\n');
-        
-        
-        
-        var result1 = file.data.toString()*/
-        res.write(send)
-    }
+    res.write(send)
     res.end()
 } )
 
