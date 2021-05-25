cff4c8faf1621e680b6d84311a409e7db75ec3c3
Yedidya Bachar <72942868+yedidya3@users.noreply.github.com>
2021-05-25 15:55:12 +0300

    Add files via upload
    Move the edit to view

---
+++ view/view.js
@@ -0,0 +1,35 @@
+
+
+
+function edit(res, JSONmessege){
+
+    let jsonString2 = JSONmessege;
+
+    let send = ""
+    for (var key in jsonString2) {
+        let counter = 1;
+        if (jsonString2.hasOwnProperty(key)) {
+                
+            let arr = jsonString2[key].toString().split(",");
+            send += "Attribute " +key+": \n";
+            let check = 0;
+            for(var i in arr){
+                if(check === 0){
+                    send +=`anomaly number: ${counter} -> from: ${arr[i]} `
+                    counter++;
+                }
+                else{
+                    send +="to: " + arr[i] +"\n";
+                }
+                check = (check+1)%2            
+            }
+            send +="\n"
+
+        }
+    }
+        
+    res.write(send)
+    res.end()
+    return
+} 
+module.exports = { edit };
